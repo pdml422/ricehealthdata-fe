55ce4888c632476e9c15d296c3287f16e93c27c7
Nam <nhnam2712@gmail.com>
2023-12-10 22:03:19 +0700

    Fix align text from LoginForm

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -25,13 +25,15 @@ const StyledForm = styled(Form)`
     width: 100%;
     margin-bottom: 1rem;
   }
+
+  .signup-link {
+    float: right;
+  }
 `
 
 const Login = () => {
-  const navigate = useNavigate()
-  const [cookies, setCookie] = useCookies(['accessToken'])
-
-  console.log(cookies)
+    const navigate = useNavigate()
+    const [cookies, setCookie] = useCookies(['accessToken'])
 
     const onFinish = async (values) => {
         try {
@@ -48,66 +50,68 @@ const Login = () => {
         }
     }
 
-  return (
-      <Wrapper>
-        <Card>
-          <h1>Login</h1>
-          <StyledForm
-              name="normal_login"
-              className="login-form"
-              initialValues={{ remember: true }}
-              onFinish={onFinish}
-          >
-            <Form.Item
-                name="username"
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-              <Input
-                  prefix={<UserOutlined className="site-form-item-icon" />}
-                  placeholder="Username"
-              />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[{ required: true, message: 'Please input your Password!' }]}
-            >
-              <Input
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  type="password"
-                  placeholder="Password"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Form.Item
-                  name="remember"
-                  valuePropName="checked"
-                  noStyle
-              >
-                <Checkbox>Remember me</Checkbox>
-              </Form.Item>
+    return (
+        <Wrapper>
+            <Card>
+                <h1>Login</h1>
+                <StyledForm
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your Username!' }]}
+                    >
+                        <Input
+                            prefix={<UserOutlined className="site-form-item-icon" />}
+                            placeholder="Username"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your Password!' }]}
+                    >
+                        <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item
+                            name="remember"
+                            valuePropName="checked"
+                            noStyle
+                        >
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
 
-              <a
-                  className="login-form-forgot"
-                  href=""
-              >
-                Forgot password
-              </a>
-            </Form.Item>
+                        <a
+                            href=""
+                        >
+                            {/*Forgot password*/}
+                        </a>
+                    </Form.Item>
 
-            <Form.Item>
-              <Button
-                  type="primary"
-                  htmlType="submit"
-                  className="login-form-button"
-              >
-                Log in
-              </Button>
-              Don't have an account <Link to="/register">Sign Up</Link>
-            </Form.Item>
-          </StyledForm>
-        </Card>
-      </Wrapper>
-  )
+                    <Form.Item>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className="login-form-button"
+                        >
+                            Log in
+                        </Button>
+                    </Form.Item>
+
+                    <Form.Item>
+                        Don't have an account ? <Link className="signup-link" to="/register">Sign Up</Link>
+                    </Form.Item>
+                </StyledForm>
+            </Card>
+        </Wrapper>
+    )
 }
 
 export default Login
