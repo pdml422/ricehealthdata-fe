b8591581633ae9aac22b79b8e7c0fc4b15ed625d
mp <minggphamm15@gmail.com>
2023-12-30 14:47:06 +0700

    Interactive map (temporary) commit

--- my-app/src/pages/UserHome.jsx
+++ my-app/src/pages/UserHome.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
-const UserHome = () => {
-    return (
-        <div>
-            <h1>User Home</h1>
-            <p>Home page body content</p>
-        </div>
-    )
-}
+import React, { useState } from 'react';
+import image from '/Users/mp/USTH/Study/Riceheo/my-app/src/map.png'
+import './UserHome.css'; // Import the CSS file for styling
+import { MapContainer, ImageOverlay, Marker, Popup } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
 
-export default UserHome
+
+const UserHome = () => {
+    const bounds = [[-90, -180], [90, 180]];
+
+    const position = [30, 40]
+
+    return (
+        <MapContainer center={[10,10]} zoom={3} scrollWheelZoom={false}>
+            <ImageOverlay
+                url={image}
+                bounds={bounds}
+            />
+
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+};
+
+export default UserHome;
